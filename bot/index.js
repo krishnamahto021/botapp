64823c3bf1ebbb68ca3397585aeb8177a57a9187
krishnamahato021 <mahtok98212@gmail.com>
2024-12-31 08:14:00 +0530

    add token

--- bot/index.js
+++ bot/index.js
@@ -1,40 +1,40 @@
 const { Telegraf, Markup } = require("telegraf");
-const TOKEN = "YOUR BOT TOKEN";
+const TOKEN = "7116718695:AAFc0ju_f7FHgcSTa4v6Tvwmkq4e-4rCZxc";
 const bot = new Telegraf(TOKEN);
 const express = require("express");
-const app = express()
-app.use(express.json())
-const web_link = "YOUR APP URL";
+const app = express();
+app.use(express.json());
+const web_link = "https://botapp-lyart.vercel.app/";
 const community_link = "https://t.me/concept_developer";
 
-
 bot.start((ctx) => {
-    const startPayload = ctx.startPayload;
-    const urlSent = `${web_link}?ref=${startPayload}`;
-    const user = ctx.message.from;
-    const userName = user.username ? `@${user.username}` : user.first_name;
-    ctx.replyWithMarkdown(`*Hey, ${userName}! I am @concept_developer , Welcome to CDPTap!*
+  const startPayload = ctx.startPayload;
+  const urlSent = `${web_link}?ref=${startPayload}`;
+  const user = ctx.message.from;
+  const userName = user.username ? `@${user.username}` : user.first_name;
+  ctx.replyWithMarkdown(
+    `*Hey, ${userName}! I am @concept_developer , Welcome to CDPTap!*
 Mine CDPTap cryptocurrency easily and earn CDPTap tokens.
 
 Start mining now and be among the biggest players earning CDPtap tokens daily.
 
 Got friends, relatives, co-workers?
 Bring them all into the game.
-More squad power, more CDPtap tokens.`, {
-        reply_markup: {
-            inline_keyboard: [
-              [{ text: "👋 Start now!", web_app: { url: urlSent } }],
-              [{ text: "Join our Community", url: community_link }]
-            
-            ],
-            in: true
-        },
-    });
-  });
+More squad power, more CDPtap tokens.`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "👋 Start now!", web_app: { url: urlSent } }],
+          [{ text: "Join our Community", url: community_link }],
+        ],
+        in: true,
+      },
+    }
+  );
+});
 
-  
-  bot.launch();
-  
+bot.launch();
+
 app.listen(3000, () => {
-    console.log("server is me and now running")
-})
+  console.log("server is me and now running");
+});
